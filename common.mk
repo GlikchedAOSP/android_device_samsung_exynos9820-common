37ff3fc60bc03dd0ec7730ee48e2530df134c3fd
glikched <florianvictorcristea@gmail.com>
2023-01-20 07:30:07 +0000

    RemovePackages: Remove Unwanted Packages

--- common.mk
+++ common.mk
@@ -267,6 +267,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/linker/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# Remove Unwanted Packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.4.vendor:64 \
